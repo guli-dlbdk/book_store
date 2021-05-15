c8bd4665228eedf77d115860f9a6a0ae7a610979
guli-dlbdk <gulistandalbudak@gmail.com>
2021-05-15 23:28:16 +0300

    min fix and cleaning

--- applications/run.py
+++ applications/run.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
-# -*- coding:utf-8 -*-
 
 from app import app
 
 if __name__ == '__main__':
-    # dummy certificate
-    app.run(host='0.0.0.0', port=5000, debug=True, ssl_context='adhoc')
+    # dummy certificate -- pip3 install pyopenssl
+    #app.run(host='0.0.0.0', port=5000, debug=True, ssl_context='adhoc')
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
